91d59354154957459f0cd002a1a29c317d4650e7
omari <omari.jalagania@gmail.com>
2022-07-27 19:23:38 +0400

    feat: add parallax effect

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { RedButton } from 'components';
 import Image from 'next/image';
+import { Parallax } from 'react-scroll-parallax';
 
 const HomePage: React.FC = () => {
   return (
@@ -16,6 +17,7 @@ const HomePage: React.FC = () => {
           />
         </div>
       </div>
+
       <div className='relative z-30 vertical-gradient'>
         <div className='absolute w-[300px] md:w-[800px] z-30 bottom-1/2 left-10 md:left-20 text-white'>
           <h2 className='text-sm md:text-4xl truncate lg:text-6xl  z-50'>
@@ -32,23 +34,24 @@ const HomePage: React.FC = () => {
         />
       </div>
 
-      <div className='relative z-30 vertical-gradient'>
-        <div className='absolute w-[300px] md:w-[800px] z-30 bottom-1/2 left-10 md:left-20 text-white'>
-          <h2 className='text-sm md:text-4xl truncate lg:text-6xl z-50'>
-            - I think we’re just gonna have to be secretly in love with earch
-            other and leave it that
-          </h2>
-          <p>The Royal Tenenbaums,2001 </p>
+      <Parallax translateY={[-100, 100]}>
+        <div className='relative z-30 vertical-gradient'>
+          <div className='absolute w-[300px] md:w-[800px] z-30 bottom-1/2 left-10 md:left-20 text-white'>
+            <h2 className='text-sm md:text-4xl truncate lg:text-6xl z-50'>
+              - I think we’re just gonna have to be secretly in love with earch
+              other and leave it that
+            </h2>
+            <p>The Royal Tenenbaums,2001 </p>
+          </div>
+          <Image
+            className='object-cover'
+            width={1920}
+            height={1080}
+            src='/../public/assets/tenen.png'
+            alt='cover'
+          />
         </div>
-        <Image
-          className='object-cover'
-          width={1920}
-          height={1080}
-          src='/../public/assets/tenen.png'
-          alt='cover'
-        />
-      </div>
-
+      </Parallax>
       <div className='relative z-30 vertical-gradient'>
         <div className='absolute w-[300px] md:w-[800px] z-30 bottom-1/2 left-10 md:left-20 text-white'>
           <h2 className='text-sm md:text-4xl truncate lg:text-6xl z-50'>
