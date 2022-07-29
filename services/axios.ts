963b47b0feb5690ee9a5ff4184a6d78f7315ec0c
omari <omari.jalagania@gmail.com>
2022-07-29 14:16:36 +0400

    fix: create axios instance

--- services/axios.ts
+++ services/axios.ts
@@ -1,14 +1,15 @@
 import axios from 'axios';
 
+const request = axios.create({
+  baseURL: process.env.BACKEND_URL,
+});
+
 export const registerHandler = async (data: {
   email: string;
   password: string;
   userName: string;
   repeatPassword: string;
 }) => {
-  const response = await axios.post(
-    `${process.env.BACKEND_URL}/user/register`,
-    data
-  );
+  const response = request.post(`/user/register`, data);
   return response;
 };
