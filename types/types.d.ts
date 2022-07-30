cf27a6aee28738b49e5ca98c6ff8af57c4b8ff26
omari <omari.jalagania@gmail.com>
2022-07-30 16:08:12 +0400

    chore: remove unused types from global types

--- types/types.d.ts
+++ types/types.d.ts
@@ -1,11 +1,3 @@
-declare global {
-  var _mongoClientPromise: string;
-  var clientPromise: Promise<MongoClient>;
-  function sum(a: number, b: number): number;
-}
-
-export {};
-
 export type ButtonProps = {
   name: string;
   className?: string;
