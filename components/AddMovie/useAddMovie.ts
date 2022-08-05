52b53263ff0edc0bbd6721d956421967d8fc8a5b
omari <omari.jalagania@gmail.com>
2022-08-05 14:21:26 +0400

    chore: remove comments

--- components/AddMovie/useAddMovie.ts
+++ components/AddMovie/useAddMovie.ts
@@ -1,18 +1,16 @@
 import { useFormik } from 'formik';
 import { getMovieFormInitialValue } from 'components';
 import { movieSchema } from 'schema';
-//import { useDispatch } from 'react-redux';
+
 import { useTranslate } from 'hooks';
 
 export const useAddMovie = () => {
-  //const dispatch = useDispatch();
   const { t } = useTranslate();
   const formik = useFormik({
     initialValues: getMovieFormInitialValue(),
     onSubmit: async (values) => {
       console.log(values);
       try {
-        //send data to server
       } catch (error) {
         throw error;
       }
