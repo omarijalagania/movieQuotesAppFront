8b59bb431b9e8ca0f525a1464e8eb54190555af2
omari <omari.jalagania@gmail.com>
2022-09-15 12:22:33 +0400

    fix: logged user can not reach landing page

--- components/shared/Header/useHeader.ts
+++ components/shared/Header/useHeader.ts
@@ -54,6 +54,12 @@ export const useHeader = (
   };
 
   useEffect(() => {
+    if (session && router.pathname === '/') {
+      router.push('/feed');
+    }
+  }, [router, session]);
+
+  useEffect(() => {
     if (registerResponse.status === 200) {
       setIsOpenThanks(true);
       setIsOpen(false);
